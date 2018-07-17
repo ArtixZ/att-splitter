4888ef0bef9104705b32e30868fb7300dc03e125
ArtixZ <artixzhu@gmail.com>
2018-07-16 23:01:47 -0400

    add calc for shared data money

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const folderName = './billings/';
 
 async function readFilesFromFolder(folderName) {
     const reqArray = fs.readdirSync(folderName).map(file => {
-        let dataBuffer = fs.readFileSync(`${folderName}${file}`)
-        return loadAmonth(dataBuffer, fp)
+        if(file.split('.')[1] === 'pdf') {
+            let dataBuffer = fs.readFileSync(`${folderName}${file}`)
+            return loadAmonth(dataBuffer, fp)
+        }
+        
     })
     await Promise.all(reqArray)
     console.log(fp)
--- loadAMonth.js
+++ loadAMonth.js
@@ -8,6 +8,8 @@ module.exports = function loadAMonth(dataBuffer, fp) {
     return pdf(dataBuffer).then(function(data) {
  
         var pages = data.text.split('\n').filter(item => !!item)
+        const dataRoamingPlan = data.text.match(/Wireless\s+?Group\s+?1\s+?-\s+?Usage Summary [-]?\s+\w*\s?\d?\d?(?: thru )?\w*\s?\d?\d? ([a-zA-Z &]+)\s+-/)[1].trim()
+        const dataRoamingPlanMoney = data.text.match(new RegExp(dataRoamingPlan+'\\s+(\\d+\\.\\d\\d)'))[1]
         const daterange = pages[0].match(/\d{2}(\/\d{2}){2}\s-\s\d{2}(\/\d{2}){2}/)[0].replace(/\s/g, '')
         var content = pages[0].substring(data.text.indexOf('Service Summary'), data.text.indexOf('Total New Charges'))
         var re = /\d*?\.\d*?\s\d{3}\s\d{3}-\d{4}\w*?/
@@ -19,7 +21,8 @@ module.exports = function loadAMonth(dataBuffer, fp) {
             const money = splitted[i].match(mReg)[0]
             let user = fp.findUser(number)
             if(!user) user = fp.addUser(number)
-            user.setMoneyToMonth(daterange, money)
+            const moneyNMoney = Number(money) + Number(dataRoamingPlanMoney) / splitted.length
+            user.setMoneyToMonth(daterange, moneyNMoney)
         }
     
     
